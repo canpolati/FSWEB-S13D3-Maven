2b03c5831a503a8714aae1eadcd44af8c60bf7a3
canpolati <canpolat.isik@gmail.com>
2026-09-08 01:23:34 +0300

    Person ve Wall sınıfları eklendi

---
+++ src/main/java/org/example/Person.java
@@ -0,0 +1,39 @@
+package org.example;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String email;
+    private String city;
+    private boolean isStudent;
+
+    public Person(String firstName, String lastName, int age) {
+        this(firstName, lastName, age, "unknown@example.com", "Unknown", false);
+    }
+
+    public Person(String firstName, String lastName, int age, String email, String city, boolean isStudent) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.email = email;
+        this.city = city;
+        this.isStudent = isStudent;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isTeen() {
+        return age >= 13 && age <= 19;
+    }
+}
